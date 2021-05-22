4d36692de6ffa920442a354ef988d1de81ad99f4
Primoz <primoz98@gmail.com>
2021-05-22 23:58:30 +0200

    The Dog API implemented

--- PetPal/app/src/main/java/si/dragonhack/petpal/ui/fragment/DashboardFragment.kt
+++ PetPal/app/src/main/java/si/dragonhack/petpal/ui/fragment/DashboardFragment.kt
@@ -8,25 +8,39 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 import si.dragonhack.petpal.R
+import si.dragonhack.petpal.api.TheDogApi
 import si.dragonhack.petpal.data.viewmodel.DashboardViewModel
 
+
 class DashboardFragment : Fragment() {
+
+    private lateinit var queue: RequestQueue
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        //TODO TDA calls example
+        queue = Volley.newRequestQueue(this.context)
+        val tda = TheDogApi(queue)
+        tda.handle_selected_breed("beagle")
+        tda.handle_allBreeds()
+
         return root
     }
+
 }
